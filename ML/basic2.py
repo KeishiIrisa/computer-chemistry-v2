5f071b08389150f18f7794798cfbdd81e0afde24
KeishiIrisa <keishi.irisa@gmail.com>
2025-04-22 16:01:12 +0900

    Lesson1

---
+++ ML/basic2.py
@@ -0,0 +1,17 @@
+import sys
+import os.path
+import pandas as pd
+
+if __name__ == "__main__":
+    input_filename = sys.argv[1]
+
+    data = pd.read_csv(input_filename, sep='¥t')
+
+    base_name, ext = os.path.splitext(input_filename)
+    ii = 0
+    while (os.path.exists('{}_{}'.format(base_name, ii))):
+        ii += 1
+    dirname = '{}_{}'.format(base_name, ii)
+    os.mkdir(dirname)
+
+    data.describe().to_csv('{}/statistics.csv'.format(dirname))
